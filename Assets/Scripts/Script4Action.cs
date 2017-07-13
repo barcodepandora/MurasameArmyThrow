9e5326750f907a5d3ffde070a600b47b523622a4
Juan Manuel Moreno <barcodepandora@iMac-de-Juan-2016.local>
2017-07-13 11:49:14 -0500

    first commit

---
+++ Assets/Scripts/Script4Action.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script4Action : MonoBehaviour {
+
+	public Transform bladeShape; // molde arma
+	public Transform blade; // arma
+	public bool isThrowing = false; // indicador está lanzándose
+
+	public int childOfBlade = 1;
+
+	// Use this for initialization
+	void Start () {
+
+		bladeShape = GetComponent<Transform> ().GetChild(childOfBlade);
+		bladeShape.gameObject.SetActive (false);
+
+//		print ("mi arma es " + bladeShape);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.B)) { // hacemos accion de usar arma con B
+
+			throwBlade ();
+		}
+
+		if (isThrowing && blade.position.x >= 5) { // desactivamos arma si esta lanzandose y llego al limite
+
+//			print ("Ya llegue al limite");
+
+			blade.gameObject.SetActive(false);
+			isThrowing = false;
+		}
+	}
+
+	// Accion de lanzar
+	void throwBlade() {
+
+//		print ("Aqui lanzamos");
+
+		if (!isThrowing) {
+	
+			blade = Instantiate(bladeShape, new Vector3(2, 0, 0), Quaternion.identity);
+			blade.gameObject.SetActive (true);
+			isThrowing = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Script4Blade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script4Blade : MonoBehaviour {
+
+	public Transform blade; // arma
+
+	public int VELOCIDAD = 10; // VELOCIDAD
+
+	// Use this for initialization
+	void Start () {
+
+		blade = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (blade.gameObject.activeInHierarchy) { // es un elemento activo
+
+// movemos
+			blade.Translate( new Vector3 (VELOCIDAD * Time.deltaTime, blade.position.y, blade.position.z) );
+
+//			print ("esta en " + blade.position.x);
+		}
+	}
+}
